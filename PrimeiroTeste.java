788e4e090bb8c8973064177918bb7392a6c54c9a
Elberth Moraes <elberth.moraes@prof.infnet.edu.br>
2021-07-21 21:46:37 -0300

    Tratamento da definicao do ano de nascimento e do calculo total

--- PrimeiroTeste.java
+++ PrimeiroTeste.java
@@ -3,18 +3,25 @@ public class PrimeiroTeste {
 	public static void main(String[] args){	
 		System.out.println("Hello, World");
 
-		if(args.length == 5){
+		if(args.length == 6){
+
 			String nome = args[0];
 			String sobrenome = args[1];
 			int idade = Integer.valueOf(args[2]);
 			float salario = Float.valueOf(args[3]);
 			boolean java = Boolean.valueOf(args[4]);
-			
-			System.out.println(nome);
-			System.out.println(sobrenome);
-			System.out.println(idade);
-			System.out.println(salario);
-			System.out.println(java);
+			int qtdeMes = Integer.valueOf(args[5]);
+
+			int anoNascimento = 2021 - idade;
+			float salarioTotal = salario * qtdeMes;
+
+			System.out.println("Nome: " + nome);
+			System.out.println("Sobrenome: " + sobrenome);
+			System.out.println("Idade: " + idade);
+			System.out.println("Ano de nascimento: " + anoNascimento);
+			System.out.println("Salario mensal: " + salario);
+			System.out.println("Recebimentos: " + salarioTotal);
+			System.out.println("Devjava: " + java);
 		} else {
 			System.out.println("Deu ruim");
 		}
